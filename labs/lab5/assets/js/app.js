d3d2302d9c956b072fa764e4444eb99d98af0aa0
AEVA KARLSRUD <karla772@newschool.edu>
2018-10-30 10:39:55 -0400

    push

--- labs/lab5/assets/js/app.js
+++ labs/lab5/assets/js/app.js
@@ -1,4 +1,12 @@
 $('.nav-item').click(function() {
-  console.log('you clicked me');
+  var button_name = $(this).data('button');
+  console.log(button_name);
+  $('.content').addClass(button_name);
+
+  $('.content').attr('class', 'column content');
+  $('.content').addClass(button_name);
+
+  $('this').addClass('active');
+  $('.content').addClass('button-name');
 });
 
