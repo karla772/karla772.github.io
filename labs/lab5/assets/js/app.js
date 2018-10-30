ed55e2a592ce4c3ad8b7ab4a65e77396cfd7a97e
AEVA KARLSRUD <karla772@newschool.edu>
2018-10-30 09:32:03 -0400

    more

--- labs/lab5/assets/js/app.js
+++ labs/lab5/assets/js/app.js
@@ -2,4 +2,10 @@ var name = "aeva",
     age = 19,
     can_drive = true;
 
-var dresser = ['socks', 'shirts', 'pants', 'old stuff'];
+var dresser = [['socks', 25], 'shirts', 'pants', 'old stuff'];
+
+function hello_world(){
+    alert('hello world');
+}
+
+hello_world();
