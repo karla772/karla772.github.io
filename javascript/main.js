2d66a4f4330d02bf14817d1c4f087301b4ae5c71
karla772 <karla772@newschool.edu>
2019-02-27 21:32:30 -0500

    java saving

---
+++ javascript/main.js
@@ -0,0 +1,73 @@
+var myName = 'Aeva';
+console.log('My name is', myName);
+
+var favNumber = 7;
+console.log('My favorite number is', favNumber);
+
+var favNumberString = '7';
+console.log('My favorite number is', favNumberString);
+
+var boolean = false;
+console.log(boolean);
+
+var arrayName = ['A', 'E', 'V', 'A'];
+console.log(arrayName);
+
+var arrayData = ['Aeva', '7', 'Lab'];
+console.log(arrayData);
+
+var fullName = 'Aeva' + 'Karlsrud';
+console.log(fullName);
+
+var equalHundred = 50 + 50;
+console.log(equalHundred);
+
+console.log(arrayName[0]);
+console.log(arrayName.length);
+
+console.log('supercalifragilisticexpialidocious'.length);
+
+console.log(fullName[0].length);
+console.log(fullName[1].length);
+
+// if ('All students are here.') {
+// console.log('All students are in class.');
+// } else {
+//   console.log('Not everyone is here.')
+// };
+
+// for (var i = 0; i < arrayName.length; i++) {
+//   console.log(arrayName[i]);
+// };
+
+// var date = new Date;
+// var hour = date.getHours();
+// var minutes = date.getMinutes();
+// console.log('hour', 'minutes' );
+
+// if (hour >= 19 && hour < 22 ) {
+//   console.log('Its between 7pm and 9:40pm');
+// } else if {
+//   console.log()
+// };
+
+// var today = Date();
+// console.log('Today is')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
