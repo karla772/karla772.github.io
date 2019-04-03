16cfb4bb242f4485956fef4ea4f3a3f899714189
karla772 <karla772@newschool.edu>
2019-04-03 19:43:05 -0400

    linking toh oempage

---
+++ ama/main.js
@@ -0,0 +1,14 @@
+// $(function() {
+//   $(function() {
+//     $('.fold3').onmouseover('another-css-class');
+//   });
+
+  
+// }
+function hover(element) {
+  element.setAttribute('src', './assets/bestfriend.jpg');
+}
+
+function unhover(element) {
+  element.setAttribute('src', './assets/fold3.jpg');
+}
