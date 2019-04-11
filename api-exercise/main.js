62fd31df78137ff5dcc6f1b24bcc13f4af383b17
karla772 <karla772@newschool.edu>
2019-04-10 20:58:41 -0400

    api images

--- api-exercise/main.js
+++ api-exercise/main.js
@@ -1,9 +1,21 @@
-$.get('http://api.openweathermap.org/data/2.5/weather?q=New+York&appid=0a1f10e1496e0fb16bdc8e0d78940ea5', function(data) {
+$.get('http://api.openweathermap.org/data/2.5/weather?zip=10003&appid=0a1f10e1496e0fb16bdc8e0d78940ea5', function(data) {
   // The get request fires a callback function when the API request finishes
   console.log(data);
+  window.data
   // The data object contains all the information returned in the API
   var weather = data.weather[0];
   // In this case, the most interesting bit of data is in the weather key
   // Now do something interesting with the data!
   $('body').html('The weather is ' + weather.description);
+
+  var temp = data.main.temp;
+  var hotKelvin = 316;
+  var rgbTemp = temp / hotKelvin
+  console.log('temp', temp);
+
+  var icon = data.weather[0].icon;
+  var image = '<img src="./assets/0d1.png">' + icon + '.jpg">';
+
+
+  $('body').append(image);
 });
