70aad12793aaa2cfc3962d59bc28a811929b8b9b
karla772 <karla772@newschool.edu>
2019-04-17 20:32:22 -0400

    mad libs story

---
+++ madlibs/main.js
@@ -0,0 +1,9 @@
+$.get('https://www.karla772.com/api.json', function(data) {
+  console.log(data);
+  var adjective1 = data.adjective1;
+
+  $('word-adjective-1').html(adjective1);
+  
+}).fail(function() {
+  console.log('Error in GET request. Handle the error gracefully.');
+});
