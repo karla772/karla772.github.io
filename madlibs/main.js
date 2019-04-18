d8969054ce1002b0761f5689f920ed76e1576153
karla772 <karla772@newschool.edu>
2019-04-17 20:40:16 -0400

    new web address

--- madlibs/main.js
+++ madlibs/main.js
@@ -1,9 +1,10 @@
-$.get('https://www.karla772.com/api.json', function(data) {
+// https://karla772.github.io/api.json
+$.get('https://karla772.github.io/api.json', function(data) {
   console.log(data);
   var adjective1 = data.adjective1;
 
-  $('word-adjective-1').html(adjective1);
-  
+  $('.adjective1').html(adjective1);
+
 }).fail(function() {
   console.log('Error in GET request. Handle the error gracefully.');
 });
